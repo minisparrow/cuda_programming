cba9d9dd1d8b714d99798ae5d1f54f46f701f39b
junli <junli.westwell-lab.com>
2017-12-23 15:17:15 +0800

    defineGridBlock

---
+++ stepsby/2gridBlock/build/CMakeFiles/defineGridBlock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/defineGridBlock.dir/defineGridBlock_generated_defineGridBlock.cu.o"
+  "defineGridBlock.pdb"
+  "defineGridBlock"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/defineGridBlock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
