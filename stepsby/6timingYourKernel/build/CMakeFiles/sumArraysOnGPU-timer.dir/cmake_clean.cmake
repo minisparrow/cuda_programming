96b47f27bbd38b6aaf46f54928895e8ed965e11c
junli <junli.westwell-lab.com>
2018-01-03 18:20:01 +0800

    timeing your cuda kernel

---
+++ stepsby/6timingYourKernel/build/CMakeFiles/sumArraysOnGPU-timer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sumArraysOnGPU-timer.dir/sumArraysOnGPU-timer_generated_sumArraysOnGPU-timer.cu.o"
+  "sumArraysOnGPU-timer.pdb"
+  "sumArraysOnGPU-timer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sumArraysOnGPU-timer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
