13c93c092449c14fde224ae0de0714c05d674740
junli <junli.westwell-lab.com>
2018-01-03 15:54:35 +0800

    chapter02 03 04

---
+++ stepsby/3checkDimension/build/CMakeFiles/checkDimension.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkDimension.dir/checkDimension_generated_checkDimension.cu.o"
+  "checkDimension.pdb"
+  "checkDimension"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/checkDimension.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ stepsby/4checkThreadIdx/build/CMakeFiles/checkThreadIdx.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkThreadIdx.dir/checkThreadIdx_generated_checkThreadIndex.cu.o"
+  "checkThreadIdx.pdb"
+  "checkThreadIdx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/checkThreadIdx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
